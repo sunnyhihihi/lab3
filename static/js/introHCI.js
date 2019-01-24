71b9191ecfd437ea3b9caa8a9415864deaf40dda
Sunny Sun <zes004@ucsd.edu>
2019-01-24 10:08:50 -0800

    yay done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,31 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("PLease wait...");
+		$(".jumbotron p").addClass("active");
+
 	});
+
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#ff69b4");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).fadeToggle();
+    }
+
+
+
 }
